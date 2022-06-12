48cac3e24161b94c56516d8abeb7821bf64b3f33
KRAF10oP <88928954+KRAF10oP@users.noreply.github.com>
2022-06-12 23:14:49 +0530

    Add files via upload

---
+++ commands/2react.js
@@ -0,0 +1,6 @@
+module.exports = [{
+name: "TEAM NAME -",
+aliases: ['Team Name -', 'Team Name-', 'team name -', 'TEAM NAME-', 'team name-', 'team ', 'Team ', 'TEAM', 'team -', 'team1'],
+nonPrefixed: true,
+code:'$addCmdReactions[$if[$mentionedUsersCount>=$getServerVar[tctags2];982999843767210026;956514158759776256]]  $onlyIf[$isBot[$mentioned[1;no]]!=true;<@$authorID>, You mentioned a bot which is not acceptable.] $onlyIf[$isBot[$mentioned[2;no]]!=true;<@$authorID>, You mentioned a bot which is not acceptable.]         $onlyIf[$isBot[$mentioned[3;no]]!=true;<@$authorID>, You mentioned a bot which is not acceptable.]            $onlyIf[$isBot[$mentioned[4;no]]!=true;<@$authorID>, You mentioned a bot which is not acceptable.]                           $onlyIf[$getServerVar[tagcheck2]==$channelID;]                         $suppressErrors[An Error Occured]'
+}]
