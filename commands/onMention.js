1706a294fb287b884b38c42169e089d69b1ce6f6
KRAF10oP <88928954+KRAF10oP@users.noreply.github.com>
2022-06-13 16:37:24 +0530

    Update onMention.js

--- commands/onMention.js
+++ commands/onMention.js
@@ -1,16 +1,16 @@
 module.exports = [{
-name: "<@945533660654882836>",
+name: "<@$clientID>",
 $if: "v4",
 nonPrefixed: true,
 code: `
 $color[1;#2F3136]
-$author[1;Scrims Mod;$userAvatar[938345769486925885]]
-$description[1;**Hello <@$authorID>, I am <@945533660654882836> i was made to help esport servers by my commands.
-My prefix for this server is \`.\`
+$author[1;$userTag[$clientID];$userAvatar[$clientID]]
+$description[1;**Hello <@$authorID>, I am <@$clientID> i am a custom instance of [flank bot](https://dsc.gg/scrimsmodinv)
+My prefix for this server is \`a!\`
 
 About Me -
 I was made by Krypto & Yash**]
-$footer[1;Type .help to see my available commands]
+$footer[1;Type a!help to see my available commands]
 $addButton[1;Invite Me!;5;https://dsc.gg/scrimsmodinv]
 $addButton[1;Support Server!;5;https://discord.gg/9V5Eb8W8pT]
 $suppressErrors[An error occured while executing this command please report this issue on our support server. https://discord.gg/9V5Eb8W8pT]
