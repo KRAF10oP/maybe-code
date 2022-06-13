174b5ec63c10d7c83dd00a0653ae5b0bbe3a75a5
KRAF10oP <88928954+KRAF10oP@users.noreply.github.com>
2022-06-13 16:33:55 +0530

    Delete 2react.js

--- 2react.js
+++
@@ -1,5 +0,0 @@
-module.exports = [{
-name: "",
-nonPrefixed: true,
-code:'$addCmdReactions[$if[$mentionedUsersCount>=$getServerVar[tctags2];982999843767210026;956514158759776256]]  $onlyIf[$isBot[$mentioned[1;no]]!=true;] $onlyIf[$isBot[$mentioned[2;no]]!=true;]         $onlyIf[$isBot[$mentioned[3;no]]!=true;]            $onlyIf[$isBot[$mentioned[4;no]]!=true;]                           $onlyIf[$getServerVar[tagcheck2]==$channelID;]                         $suppressErrors[An Error Occured]'
-}]
