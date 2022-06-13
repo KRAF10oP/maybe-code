aaec3040c437b72a2120674bc0df22f1cc2f3a6c
KRAF10oP <88928954+KRAF10oP@users.noreply.github.com>
2022-06-13 16:43:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const keepAlive = require('./server.js')
 const aoijs = require("aoi.js")
 const bot = new aoijs.Bot({
-token: "token: "OTg1NTg3MDI4NDA0MDc2NTg0.GsZhJ6._Vx8iQCTY6nGGj7WqSnnahkaVW-XlaEpcQguZI", //Discord Bot Token
+token: "OTg1NTg3MDI4NDA0MDc2NTg0.GsZhJ6._Vx8iQCTY6nGGj7WqSnnahkaVW-XlaEpcQguZI", //Discord Bot Token
 prefix: "a!", //Discord Bot Prefix
 intents: "all" //Discord Intents
 })
